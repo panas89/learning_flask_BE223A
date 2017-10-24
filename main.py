2306b609228cfc7b1e819253936ebd46c2dd6604
Panayiotis Petousis <pp89@ucla.edu>
2017-10-24 16:40:05 -0700

    Web form

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 
+poll_data = {'Question':'How difficult is this class?',
+             'fields':['Easy','Not so Easy','Average','Above average','Difficult']
+                                  }
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
@@ -13,6 +17,11 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html',name=name)
+
+
+@app.route('/polls')
+def polls():
+    return render_template('polls.html',data=poll_data)
 
 
 if __name__ == "__main__":
